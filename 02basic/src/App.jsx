dada871929ff9c86cc3ea96bfda4b85d99a53de5
NASIRUDDIN MANIHAR <68464223+nasiruddinmanihar@users.noreply.github.com>
2024-07-17 12:03:01 +0530

    Add files via upload

---
+++ 02basic/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from "./Chai"
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+  <Chai> </Chai>
+  <h1>hello world</h1>
+  </>
+  )
+}
+
+export default App
